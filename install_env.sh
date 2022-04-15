2d34942e621ae7e2ff7dffd7ece44bc00f42277f
ChunjunHu <huchunjun0725@gmail.com>
2022-04-15 00:24:38 -0400

    tt

--- install_env.sh
+++ install_env.sh
@@ -9,7 +9,7 @@ cd /home/ec2-user
 sudo yum update
 sudo yum install ruby -y
 sudo yum install wget
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
